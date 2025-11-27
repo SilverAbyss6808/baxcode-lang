0c07ef85cc249f12cf379db5dab5205cb2101fe7
l-baxter1 <l-baxter@onu.edu>
2025-11-26 19:22:18 -0500

    stuff to this point

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 The documentation for the language will go here once I'm done with it. Or at least have more than I have written now. It's a big process, making a language.
+
+You can view the docs [here](https://docs.google.com/document/d/1livT9JiAFK6Yp56qVn9ECTsoOd_SUX-AZmmqXV1nLGI/).
+
+Currently, to run a program, you must type in a terminal:
+    `py \path\to\compiler.py \path\to\sourcefile.bco`
---
+++ scripts/lexer.py
@@ -0,0 +1,40 @@
+
+import sys
+import traceback
+
+# goal of this file is to return a token stream to the caller
+
+class BcoLex:
+
+    known_tokens = []
+
+    def __init__(self, source_code):
+        self.source_code = source_code
+        self.line_num = 0
+
+    def iter_tokens(self):
+        for token in self:
+            pass
+    
+    def iter_lines(self):
+        for line in self.source_code.split('\n'):
+            self.line_num += 1
+            print(f'{self.line_num}     {line}')
+
+    def throw_err(self, error_message):
+        raise ValueError(f'Line {self.line_num}: {error_message}')
+
+    def tokenerize(self):
+        pass
+
+
+try:
+
+    with open(sys.argv[1], 'rt') as f:
+        code = f.read()
+    codefile = BcoLex(code)
+    codefile.tokenerize()
+
+except ValueError as v: print(v)
+except Exception: traceback.print_exc()
+
